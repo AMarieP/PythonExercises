481436d16a143237afe7acb439a9cdd3f5d9c31f
Alyssa <alyssa@kvasl.com>
2023-03-01 13:05:01 +1300

    Activities 1,2, 3

---
+++ Week_2/Activity_3.py
@@ -0,0 +1,2 @@
+# Write Python Program that contains a Linked List and perform the following 
+# operations on it. You can store any random values to the LinkedList.
---
+++ Week_2/MyList.py
@@ -0,0 +1,14 @@
+#Create a shopping list which allows user inputs
+running = True
+
+
+while running == True:
+    print("")
+
+
+shoppingList = []
+
+def PrintList(List):
+    print(List)
+
+PrintList(shoppingList)
